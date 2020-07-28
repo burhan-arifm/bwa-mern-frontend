fc1d8ec45fbeec852f0d59bf55f0be59bfad07b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-28 01:05:16 +0000

    Deploy burhan-arifm/bwa-mern-frontend to github.com/burhan-arifm/bwa-mern-frontend.git:gh-pages

---
+++ precache-manifest.c7ffaeaaff8d4c015d64ab73ad8d8bbf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa4565032d066eb979b2fbdb704a574",
+    "url": "/bwa-mern-frontend/index.html"
+  },
+  {
+    "revision": "3ae22b10afe0c5d6562a",
+    "url": "/bwa-mern-frontend/static/css/main.c17db8e9.chunk.css"
+  },
+  {
+    "revision": "cec5edf4dad5fc95afa4",
+    "url": "/bwa-mern-frontend/static/js/2.bbde6ab8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bwa-mern-frontend/static/js/2.bbde6ab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae22b10afe0c5d6562a",
+    "url": "/bwa-mern-frontend/static/js/main.5d2a2016.chunk.js"
+  },
+  {
+    "revision": "26f28a0afabbe78f25f6",
+    "url": "/bwa-mern-frontend/static/js/runtime-main.c57fd42c.js"
+  },
+  {
+    "revision": "bc95ea7081dab6207da118aa85e6c3fa",
+    "url": "/bwa-mern-frontend/static/media/ic_cities.bc95ea70.svg"
+  },
+  {
+    "revision": "109e18e8160fb2c8887bfc75e9cbc2c6",
+    "url": "/bwa-mern-frontend/static/media/ic_traveler.109e18e8.svg"
+  },
+  {
+    "revision": "bb259d86e7904d0ce5931ebee9924b69",
+    "url": "/bwa-mern-frontend/static/media/ic_treasure.bb259d86.svg"
+  },
+  {
+    "revision": "c21ecd2e840094b2feb7150c6b117056",
+    "url": "/bwa-mern-frontend/static/media/img-hero.c21ecd2e.jpg"
+  }
+]);
